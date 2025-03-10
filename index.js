b5aaffd36098a9c42c1f9a8b215939e74e1a593f
Abdullahi <abdullahiibrashair@gmail.com>
2025-03-10 11:15:16 +0300

    phase-1-using-array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
 
 // Code your solution here
+const batteryBatches = [3, 4, 3, 3, 4, 5, 3, 2, 2, 2]; 
+
+
+const totalBatteries = batteryBatches.reduce((acc, batch) => acc + batch, 0);
+
+
+console.log(totalBatteries);
